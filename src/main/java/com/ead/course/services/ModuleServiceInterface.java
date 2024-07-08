99e25de64c7c3963c107c8a2829072ee7aa15198
igor-lourenco <igor.lourencosantos@gmail.com>
2024-07-08 14:54:46 -0300

    Implementação do delete em cascada no ModuleService

--- src/main/java/com/ead/course/services/ModuleServiceInterface.java
+++ src/main/java/com/ead/course/services/ModuleServiceInterface.java
@@ -1,7 +1,10 @@
 package com.ead.course.services;
 
+import com.ead.course.models.ModuleModel;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface ModuleServiceInterface {
+
+    void delete(ModuleModel moduleModel);
 }
